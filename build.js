38b926e3ee2da773e31279e2d93f7a8ef8364368
ItsLap <74074061+ItsLap@users.noreply.github.com>
2025-04-22 21:47:38 +0100

    First Push

---
+++ build.js
@@ -0,0 +1,27 @@
+// To be ran in Node.js
+const fs = require("fs");
+const path = require("path");
+const JSON5 = require("json5");
+
+let dirs = fs.readdirSync("pkgs");
+let pkgList = [];
+let pkgListOld = [];
+
+for (let dir of dirs) {
+  const pkgs = fs.readdirSync(path.join("pkgs", dir));
+  for (const pkg of pkgs) {
+    const pkgData = Object.assign(
+      JSON5.parse(
+        fs.readFileSync(`./pkgs/${dir}/${pkg}/meta.json5`, {
+          encoding: "utf8",
+        })
+      ),
+      { id: `${dir}/${pkg}` }
+    );
+    pkgList.push(pkgData);
+    pkgListOld.push(`${dir}/${pkg}`);
+  }
+}
+
+fs.writeFileSync("listing.json", JSON.stringify(pkgListOld));
+fs.writeFileSync("listing_new.json", JSON.stringify(pkgList));
